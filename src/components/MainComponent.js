0a73db3cddd43eff490763ee66753b9cd2415925
ZapdasMYF <yassar.farooq@zapdas.co>
2020-09-14 00:55:22 +0500

    header-fotter-component-added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 //function App()
 class Main extends Component {
@@ -29,21 +31,17 @@ class Main extends Component {
     console.log('Main Render Invoke')
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
 
         {/* up to down ----- onClick ko as a parameter pass kr rhe hain parameter m jo hoga hmausy Menu m props k through access kr lain ge */}
         {/* OR */}
         {/* down to up ----- Menu sy onClick k data idr aya hai*/}
+        {/* dishId kaise aye ider */}
         <Menu dishes={this.state.dishes} onClick={(dishId) => 
             this.onDishSelect(dishId)}/>
-            
-            {/* dishId kaise aye ider */}
-        
+      
         <DishDetail dishdetail={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] }/>
+        <Footer />
       </div>
     );
   }
